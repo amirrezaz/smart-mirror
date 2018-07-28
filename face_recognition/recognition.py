35ec366428ef7742b53a129143828c8dca77838b
Amir Zareian <zareiana@TMGM-C02SQ3U6FVH4.connect>
2018-07-28 21:38:16 +0100

    refactor face recog

--- face_recognition/recognition.py
+++ face_recognition/recognition.py
@@ -22,8 +22,8 @@ class RecognitionThread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
 
-    def __init__(self):
-        super(RecognitionThread, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(RecognitionThread, self).__init__(*args, **kwargs)
         self._stop_event = threading.Event()
 
     def stop(self):
