5f5262df55c402a75dd723533969843a745da426
Amir Zareian <zareiana@TMGM-C02SQ3U6FVH4.connect>
2018-07-29 19:51:58 +0100

    fix recording

--- camera.py
+++ camera.py
@@ -9,7 +9,7 @@ from subprocess import call
 from picamera import PiCamera
 from face_recognition.recognition import recognition
 import time
-
+import subprocess
 
 capture = Blueprint('capture', __name__, template_folder='templates')
 @capture.route('/capture/')
@@ -61,20 +61,22 @@ def camera_record():
     camera.resolution = (1024, 768)
     camera.start_preview()
 
-    camera.start_recording('video.avi')
+    camera.start_recording('video.h264')
     camera.wait_recording(10)
     camera.stop_recording()
     camera.stop_preview()
     camera.close()
     recognition.start()
 
+    subprocess.call(['MP4Box', '-add', 'video.h264', 'video.mp4'])
+
     dbx = dropbox.Dropbox(access_token)
 
-    with open("video.avi", "rb") as videoFile:
+    with open("video.mp4", "rb") as videoFile:
         f = videoFile.read()
         dbx.files_upload(
             f,
-            '/{app_folder}/{filename}.avi'.format(
+            '/{app_folder}/{filename}.mp4'.format(
                 app_folder=app_folder,
                 filename=datetime.now().strftime('%s')
             )
