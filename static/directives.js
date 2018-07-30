988d508dcad877a3da7e179837950bf8f13f8761
Amir Zareian <zareiana@TMGM-C02SQ3U6FVH4.local>
2018-07-30 20:39:19 +0100

    fix power on/off

--- static/directives.js
+++ static/directives.js
@@ -303,6 +303,7 @@ app.directive('power', ['$http','$interval',function ($http, $interval) {
             scope.power = function($event, type) {
 
                 console.log($event.currentTarget.nodeName);
+                console.log(this);
 
                 $http({
                     method : "GET",
